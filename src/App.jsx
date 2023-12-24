6ebca87cd63665140f97b1a3aa26d1b7403cfc74
encodedevv <timothy.onyeacholam@nxghubdigitech.onmicrosoft.com>
2023-12-24 14:09:04 +0100

    project initialization

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Switch, Route, Link } from 'react-router-dom';
+import { Layout, Typography, Space } from 'antd';
+
+const App = () => {
+  return (
+    <div className='app'>
+      <h1>Cryptoverse</h1>
+    </div>
+  )
+}
+
+export default App
